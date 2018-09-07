d959c50ab5c75212baaf381415c19a4c4c19ee93
Philippe H <36929410+phlhg@users.noreply.github.com>
2018-09-07 20:02:09 +0200

    Disable autocapitalize in the username input

--- php/app/views/login/form.php
+++ php/app/views/login/form.php
@@ -1,6 +1,6 @@
 <form action="" method="POST">
     <input name="ph_login_tkn" type="hidden" value="<?php echo \Helpers\Token::get(); ?>" />
-    <input name="ph_login_username" required value="<?php echo $view->v->login_used_username; ?>" type="text" placeholder="Nutzername oder E-Mail"  pattern="([A-Za-z\._\-$]{3,25})|([a-z\.]+@[a-z]+\.[a-z]{1,4})" title="Gib eine gültige E-Mail oder eine gültigen Nutzernamen mit 3 bis 25 Zeichen ein." />
+    <input name="ph_login_username" required autocorrect="off" autocapitalize="none" value="<?php echo $view->v->login_used_username; ?>" type="text" placeholder="Nutzername oder E-Mail"  pattern="([A-Za-z\._\-$]{3,25})|([a-z\.]+@[a-z]+\.[a-z]{1,4})" title="Gib eine gültige E-Mail oder eine gültigen Nutzernamen mit 3 bis 25 Zeichen ein." />
     <input name="ph_login_password" required value="<?php echo $view->v->login_used_password; ?>" type="password" placeholder="Passwort" />
     <?php if(!empty($view->v->login_form_error)){ ?>
         <span class="ph_login_error"><i class="material-icons">error</i> <?php echo $view->v->login_form_error; ?></span>
