ce3738070cd2ad5bc3e904bedcfe45b95530feb5
Philippe H <36929410+phlhg@users.noreply.github.com>
2018-09-07 19:52:40 +0200

    Add properties (E-Mail,conditions,etc.)

--- php/config/config.php
+++ php/config/config.php
@@ -9,9 +9,13 @@
     $_CONFIG = [];
 
     //APPLICATION
+    $_CONFIG["application_domain"] = "antisocial.vs";
     $_CONFIG["application_title"] = "ANTISOCIAL";
     $_CONFIG["application_version"] = "1.0.0";
     $_CONFIG["application_version_stage"] = false; //For future error reporting
+
+    //CONDITIONS
+    $_CONFIG["conditions_version_last"] = 1536078668; 
 
     //DEFAULT
     $_CONFIG["default_meta_title_appendix"] = " | ANTISOCIAL";
@@ -19,6 +23,13 @@
     $_CONFIG["default_meta_menu"] = true;
     $_CONFIG["default_meta_image"] = "image";
     $_CONFIG["default_meta_themecolor"] = "rgb(250,200,0)";
+
+    //E-MAIL
+    $_CONFIG["email_noreply"] = "noreply@phlhg.ch";
+    $_CONFIG["email_noreply_name"] = "ANTISOCIAL";
+
+    //HASH
+    $_CONFIG["hash_salt"] = "ph_73238813";
 
     //USER / CLIENT
     $_CONFIG["user_guest_allow"] = false;
