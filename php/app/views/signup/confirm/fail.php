67731b480c8839f33f128515b88dab49e2e45688
Philippe H <36929410+phlhg@users.noreply.github.com>
2018-09-10 17:32:35 +0200

    Fix a presentation error

--- php/app/views/signup/confirm/fail.php
+++ php/app/views/signup/confirm/fail.php
@@ -1,4 +1,4 @@
-<section class="fullscreen">
+<section class="min-fullscreen">
     <div class="ph_text">
         <h1>Ungültig</h1>
         <p>Account konnte nicht bestätigt werden, da der Code ungültig ist.</p>
--- php/app/views/signup/confirm/success.php
+++ php/app/views/signup/confirm/success.php
@@ -1,4 +1,4 @@
-<section class="fullscreen">
+<section class="min-fullscreen">
     <div class="ph_text">
         <h1>Bestätigt</h1>
         <p>Dein Account wurde bestätigt. Du kannst dich jetzt anmelden.</p>
