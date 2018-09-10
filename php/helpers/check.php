a68cda7d9887c0a9a31a91ef922c827a3bdb218b
Philippe H <36929410+phlhg@users.noreply.github.com>
2018-09-10 17:33:43 +0200

    Covert pattern to a check-class

---
+++ php/helpers/check.php
@@ -0,0 +1,25 @@
+<?php
+
+    namespace Helpers;
+
+    class Check {
+
+        public static function username($name){
+            if(preg_match('/^[A-Za-z0-9\._\-$]{3,25}$/i',$name) === 1){ return true; }
+            return false;
+        }
+
+        public static function email($email){
+            if(filter_var($email, FILTER_VALIDATE_EMAIL)){ return true; }
+            return false;
+        }
+
+        public static function emailProvider($email){
+            $ex = explode("@",$email);
+            if(count($ex) < 2){ return false; }
+            return checkdnsrr($ex[1]);
+        }
+
+    }
+
+?>
