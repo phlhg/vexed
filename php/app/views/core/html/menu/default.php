b9e18b8f042441aba9f8a2e23f41626dbef26b47
PHLHG <36929410+phlhg@users.noreply.github.com>
2018-11-03 21:47:46 +0100

    Redesign menu

--- php/app/views/core/html/menu/default.php
+++ php/app/views/core/html/menu/default.php
@@ -1,19 +1,23 @@
 <!--MENU-->
 <header class="<?=(isset($view->v->page_menu_class) ? $view->v->page_menu_class : '')?>"><div class="ph_inner"><!--
-    --><div class="ph_mham"><!--
-        --><span></span><span></span><span></span><span></span><span></span><!--
-    --></div><!--
+    --<div class="ph_mham"><span></span><span></span><span></span><span></span><span></span></div>--
+    --><a href="/" class="ph_logo_wrapper">
+            <img src="/img/icons/logo.svg" />
+    </a><!--
     --><div class="ph_menur">
-        <a href="/search/" class="ph_menu_point msearch"><i class="material-icons">search</i></a>
         <?php if($view->client->isLoggedIn()){ ?>
-            <a href="/create/" class="ph_menu_point mcreate"><i class="material-icons">add</i></a>
-            <a href="/p/<?=$view->client->name?>" class="ph_menu_point mprofile"><img src="/img/pb/<?=$view->client->id?>" /></a>
+            <a title="Start" href="/" class="ph_menu_point mhome"><i class="material-icons">home</i></a>
+            <a title="Entdecken" href="/search/" class="ph_menu_point msearch"><i class="material-icons">search</i></a>
+            <a title="Beitrag erstellen" href="/create/" class="ph_menu_point mcreate"><i class="material-icons">add</i></a>
+            <a title="Profil" href="/p/<?=$view->client->name?>" class="ph_menu_point mprofile"><img src="/img/pb/<?=$view->client->id?>" /></a>
+            <!--<a title="Abmelden" href="/logout/?re=<?=\App::$router->url?>" class="ph_menu_point mlogout"><i class="material-icons">power_settings_new</i></a>-->
         <?php } else { ?>
             <a href="/" class="ph_menu_point mprofile no_user"><i class="material-icons">person_outline</i></a>
         <?php } ?>
-        <a href="/" class="ph_logo_wrapper">
-            <img src="/img/icons/logo.svg" />
-        </a>
     </div><!--
 --></div></header>
-<?php $view->get("core/html/menu/nav"); ?>
+<div class="mobile_header">
+    <a href="/" class="ph_logo_wrapper">
+        <img src="/img/icons/logo.svg" />
+    </a>
+</div>
