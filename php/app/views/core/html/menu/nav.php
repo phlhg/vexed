57d0d1808e869cba22774d3109a9b3f7656edfeb
PHLHG <36929410+phlhg@users.noreply.github.com>
2018-11-03 13:01:04 +0100

    Remove useless links from menu

--- php/app/views/core/html/menu/nav.php
+++ php/app/views/core/html/menu/nav.php
@@ -6,13 +6,10 @@
                     <span class="ph_menu_head">Menu</span>
                     <nav><!--
                     <?php if($view->client->isLoggedIn()){ ?>
-                            --><a href="/about/"><span>Über</span></a><!--
-                            --><a href="/"><span>Feed</span></a><!--
-                            --><a href="/web/"><span>WEB</span></a><!--
-                            --><a href="/p/<?=$view->client->name?>/"><span>Mein Profil</span></a><!--
+                            --><a href="/"><span>Start</span></a><!--
+                            --><a href="/search/"><span>Entdecken</span></a><!--
                             --><a href="/Logout/"><span>Abmelden</span></a><!--
                     <?php } else { ?>
-                            --><a href="/about/"><span>Über</span></a><!--
                             --><a href="/"><span>Login</span></a><!--
                             --><a href="/signup/"><span>Registrieren</span></a><!--
                     <?php } ?>
