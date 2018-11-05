efc1021356032397eb8fb74500de0cb75002158b
PHLHG <36929410+phlhg@users.noreply.github.com>
2018-11-05 23:11:10 +0100

    Display message if feed is empty

--- php/app/controllers/home.php
+++ php/app/controllers/home.php
@@ -13,10 +13,17 @@
             $this->view->v->content = "";
 
             $feed = new \App\Models\Post\Feed();
+            if(count($feed->postlist) < 1){ return __ROUTER()->setRoute("Home/empty"); }
             foreach($feed->postlist as $id){
                 $post = new \App\Models\Post\Post($id);
                 $this->view->v->content .= $post->toHtmlFeed();
             }
+        }
+
+        public function empty($_URL){
+            $this->view("home/empty");
+            $this->view->meta->title = "Start";
+            $this->view->meta->description = "Das soziale Netzwerk. Erstelle ein Profil und trete der Community bei";
         }
         
     }
---
+++ php/app/views/home/empty.php
@@ -0,0 +1,14 @@
+<section class="fullscreen ph_separator">
+    <div class="ph_separator_half">
+        <div class="ph_text">
+            <img src="/img/grafiken/ghost.png" class="ph_fun_graphics"/>
+        </div>
+    </div>
+    <div class="ph_separator_half">
+        <div class="ph_text">
+            <h2>Gespenstisch</h2>
+            <p>leer ist es in deiner Startseite.<br/>Zeit, dass du ein paar andere Nutzern findest!</p>
+            <a href="/search/" class="ph_button main">Entdecken</a>
+        </div>
+    </div>
+</section>
